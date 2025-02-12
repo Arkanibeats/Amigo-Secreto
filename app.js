81cfd329a43412a935f8046a30674a41999bfcb7
SpacemanPolux12 <bejarchang@gmail.com>
2025-02-11 20:36:22 -0500

    Creando la funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    var inputAmigo = document.getElementById('amigo').value;
+    //console.log(inputAmigo);
+    if (inputAmigo === "") {
+        alert("Debe Escribir un nombre");
+    }else{
+        amigos.push(inputAmigo);
+        console.log(amigos);
+    }
+    
+    document.getElementById("amigo").value = "";
+    
+}
+
